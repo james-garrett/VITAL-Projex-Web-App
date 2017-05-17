c6ad016b97f5bf95a810ae977ebf6b00a17ef72a
JamesG <debbie1235@hotmail.com>
2017-05-17 14:21:21 +1000

    Copied ColorBrewer palette into a JSON array, going to use to color ticks.

--- angularjs-slider-master/demo/js/app.js
+++ angularjs-slider-master/demo/js/app.js
@@ -768,14 +768,23 @@ app.factory('Gem', ['$rootScope', '$http', 'JSONData', function($rootScope, $htt
 }]);
 
 
-app.factory('Slider', ['$rootScope', '$http', 'AnswerListener', 'JSONData', 'NotifyingService', 
-              function($rootScope, $http, AnswerListener, JSONData, NotifyingService) {
+app.factory('Slider', ['$rootScope', '$http', 'AnswerListener', 'JSONData', 'NotifyingService','ColorBrewerList', 
+              function($rootScope, $http, AnswerListener, JSONData, NotifyingService, ColorBrewerList) {
     
     var Slider = function(questionJSONData) {
       this.initialize = function() {
         this.slider_ticks_legend = {};
         this.indexChosen = JSONData.getIndex();
         this.valueSelected = 4;
+        // var colourRange = require("./././trianglify-master/lib/colorbrewer.js");
+        // require(['./././trianglify-master/lib/colorbrewer.js'], function(foo) {
+        //   var colourRange = require('./././trianglify-master/lib/colorbrewer.js');
+        // });
+        // console.log(module.exports);
+        // var dom = document.getElementsByClassName('rz-bar'); 
+        // console.log(dom);
+        console.log(ColorBrewerList.searchForColor("Reds"));
+        // dom.style.backgroundImage = 'linear-gradient(#000000, #ffffff)';
     };
 
     notify = function() {
@@ -791,6 +800,8 @@ app.factory('Slider', ['$rootScope', '$http', 'AnswerListener', 'JSONData', 'Not
       //   div.removeChild(div.firstChild);
       // }
     }
+
+
 
 
     backToMenu = function() {
@@ -845,6 +856,16 @@ app.factory('Slider', ['$rootScope', '$http', 'AnswerListener', 'JSONData', 'Not
               return (JSONData.returnQuestionJSONData()[0][JSONData.getIndex()].ValueOptions.value[v].definition);
             },
 
+            getTickColor: function (value) {
+              if (value < 3)
+                return 'red';
+              if (value < 6)
+                return 'orange';
+              if (value < 9)
+                return 'yellow';
+              return '#2AE02A';
+          },
+
             getPointerColor: function(v) {              
               AnswerListener.setInputValue(v);
               notify();
@@ -860,6 +881,8 @@ app.factory('Slider', ['$rootScope', '$http', 'AnswerListener', 'JSONData', 'Not
 
     this.initialize();
     this.setSlider(questionJSONData);
+
+    
 
   };
   return Slider;
@@ -943,6 +966,62 @@ app.service('JSONData', function() {
       setIndex: setIndex,
     };
 
+});
+
+app.service('ColorBrewerList', function() {
+  var colorList = {"color": [
+    {"color": "YlGn", "array": ["#ffffe5","#f7fcb9","#d9f0a3","#addd8e","#78c679","#41ab5d","#238443","#006837","#004529"]},
+    {"color": "YlGnBu", "array": ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]},
+    {"color": "GnBu", "array": ["#f7fcf0","#e0f3db","#ccebc5","#a8ddb5","#7bccc4","#4eb3d3","#2b8cbe","#0868ac","#084081"]},
+   {"color":  "BuGn", "array": ["#f7fcfd","#e5f5f9","#ccece6","#99d8c9","#66c2a4","#41ae76","#238b45","#006d2c","#00441b"]},
+    {"color": "PuBuGn", "array": ["#fff7fb","#ece2f0","#d0d1e6","#a6bddb","#67a9cf","#3690c0","#02818a","#016c59","#014636"]},
+    {"color": "PuBu", "array": ["#fff7fb","#ece7f2","#d0d1e6","#a6bddb","#74a9cf","#3690c0","#0570b0","#045a8d","#023858"]},
+    {"color": "BuPu", "array": ["#f7fcfd","#e0ecf4","#bfd3e6","#9ebcda","#8c96c6","#8c6bb1","#88419d","#810f7c","#4d004b"]},
+    {"color": "RdPu", "array": ["#fff7f3","#fde0dd","#fcc5c0","#fa9fb5","#f768a1","#dd3497","#ae017e","#7a0177","#49006a"]},
+    {"color": "PuRd", "array": ["#f7f4f9","#e7e1ef","#d4b9da","#c994c7","#df65b0","#e7298a","#ce1256","#980043","#67001f"]},
+    {"color": "OrRd", "array": ["#fff7ec","#fee8c8","#fdd49e","#fdbb84","#fc8d59","#ef6548","#d7301f","#b30000","#7f0000"]},
+    {"color": "YlOrRd", "array": ["#ffffcc","#ffeda0","#fed976","#feb24c","#fd8d3c","#fc4e2a","#e31a1c","#bd0026","#800026"]},
+    {"color": "YlOrBr", "array": ["#ffffe5","#fff7bc","#fee391","#fec44f","#fe9929","#ec7014","#cc4c02","#993404","#662506"]},
+    {"color": "Purples", "array": ["#fcfbfd","#efedf5","#dadaeb","#bcbddc","#9e9ac8","#807dba","#6a51a3","#54278f","#3f007d"]},
+   {"color":  "Blues", "array": ["#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5","#08519c","#08306b"]},
+   {"color":  "Greens", "array": ["#f7fcf5","#e5f5e0","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#006d2c","#00441b"]},
+   {"color":  "Oranges", "array": ["#fff5eb","#fee6ce","#fdd0a2","#fdae6b","#fd8d3c","#f16913","#d94801","#a63603","#7f2704"]},
+    {"color": "Reds", "array": ["#fff5f0","#fee0d2","#fcbba1","#fc9272","#fb6a4a","#ef3b2c","#cb181d","#a50f15","#67000d"]},
+   {"color":  "Greys", "array": ["#ffffff","#f0f0f0","#d9d9d9","#bdbdbd","#969696","#737373","#525252","#252525","#000000"]},
+   {"color":  "PuOr", "array": ["#7f3b08","#b35806","#e08214","#fdb863","#fee0b6","#f7f7f7","#d8daeb","#b2abd2","#8073ac","#542788","#2d004b"]},
+   {"color":  "BrBG", "array": ["#543005","#8c510a","#bf812d","#dfc27d","#f6e8c3","#f5f5f5","#c7eae5","#80cdc1","#35978f","#01665e","#003c30"]},
+   {"color":  "PRGn", "array": ["#40004b","#762a83","#9970ab","#c2a5cf","#e7d4e8","#f7f7f7","#d9f0d3","#a6dba0","#5aae61","#1b7837","#00441b"]},
+  {"color":   "PiYG", "array": ["#8e0152","#c51b7d","#de77ae","#f1b6da","#fde0ef","#f7f7f7","#e6f5d0","#b8e186","#7fbc41","#4d9221","#276419"]},
+   {"color":  "RdBu", "array": ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"]},
+   {"color":  "RdGy", "array": ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#ffffff","#e0e0e0","#bababa","#878787","#4d4d4d","#1a1a1a"]},
+   {"color":  "RdYlBu", "array": ["#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffbf","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695"]},
+   {"color":  "Spectral", "array": ["#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#ffffbf","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2"]},
+   {"color":  "RdYlGn", "array": ["#a50026","#d73027","#f46d43","#fdae61","#fee08b","#ffffbf","#d9ef8b","#a6d96a","#66bd63","#1a9850","#006837"]}
+  ]};
+
+  var returnColorList = function() {
+    return colorList;
+  }
+
+  var searchForColor = function(color) {
+    console.log(colorList.color[0]);
+    Object.keys(colorList.color).forEach(function(elem, index) {
+        
+        var colorName = colorList.color[index].color;
+        console.log(colorName, index, color, colorName == color);
+        if(colorName == color) {
+          console.log(colorName, typeof(elem), color);
+          console.log(colorList.color[index].array[0]);
+          return colorList.color[index].array[0];
+        }
+        // console.log(index + " " + elem + " " + (elem.toString() === color) + " " + elem.toString());
+      });
+  }
+
+  return {
+    returnColorList: returnColorList,
+    searchForColor: searchForColor,
+  };
 });
 
 app.service('PHPData', function() {
